5cf4a796d4c18560c44db2fa69be5d23ef2b0c72
Intex Digital <tfmcode@gmail.com>
2025-05-28 13:33:56 -0400

    v2

--- guia-camiones/src/components/home/Services.tsx
+++ guia-camiones/src/components/home/Services.tsx
@@ -5,7 +5,7 @@ const Services = () => {
       <div className="absolute top-0 left-1/2 -translate-x-1/2 w-24 h-1 bg-rose-600 rounded-full"></div>
 
       <h2 className="text-3xl md:text-4xl font-extrabold text-gray-900 text-center mb-14">
-        Servicios Más Demandados
+        Servicios Más Solicitados
       </h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
